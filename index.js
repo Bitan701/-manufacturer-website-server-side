e17bcdaab2b4a92eb20315026cacf03acd49c53c
bitan701 <bitan701@gmail.com>
2022-05-24 15:40:54 +0600

    make a user admin

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ async function run() {
 		})
 
 		// Keep user information in database
-		app.put('/user/:email', async (req, res) => {
+		app.put('/users/:email', async (req, res) => {
 			const email = req.params.email
 			const user = req.body
 			const filter = { email: email }
@@ -41,6 +41,22 @@ async function run() {
 			}
 			const result = await userCollection.updateOne(filter, updateDoc, options)
 			res.send(result)
+		})
+
+		// make user admin
+		app.put('/users/admin/:email', async (req, res) => {
+			const email = req.params.email
+			const filter = { email: email }
+			const updateDoc = {
+				$set: { role: 'admin' },
+			}
+			const result = await userCollection.updateOne(filter, updateDoc)
+			res.send(result)
+		})
+
+		app.get('/users', async (req, res) => {
+			const users = await userCollection.find().toArray()
+			res.send(users)
 		})
 	} finally {
 	}
